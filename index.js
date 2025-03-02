e19ca3c2f638b79ca2d17e1da527eb1ab34c03db
Brian Payne <payne.in.the.brian@gmail.com>
2025-03-01 20:55:16 -0600

    ex 3.2

--- index.js
+++ index.js
@@ -32,6 +32,13 @@ app.get('/api/persons', (request, response) => {
   response.json(persons)
 })
 
+app.get('/info', (request, response) => {
+  const date = new Date();
+  response.send(`<h3> Phonebook has info for ${persons.length} people </h3>
+                 <h3>${date}</h3>`
+                )
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
