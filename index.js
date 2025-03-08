7f37033e2d55372d184f164eaa38019a47a0bfe8
Brian Payne <payne.in.the.brian@gmail.com>
2025-03-07 23:06:38 -0600

    ex 3.11

--- index.js
+++ index.js
@@ -8,9 +8,11 @@ morgan.token('body', function getId (req) {
   return `${JSON.stringify(req.body)}`
 })
 
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body '))
+app.use(express.static('dist'))
 app.use(cors())
 app.use(express.json())
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body '))
+
 
 
 
