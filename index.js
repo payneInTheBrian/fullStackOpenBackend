3ec6c9ba7fb0007d3d9390a4cefdb54c7b02d70d
Brian Payne <payne.in.the.brian@gmail.com>
2025-03-10 01:15:24 -0500

    ex 3.13

--- index.js
+++ index.js
@@ -2,7 +2,37 @@ const express = require('express')
 const app = express()
 const morgan = require('morgan')
 const cors = require('cors')
+const mongoose = require('mongoose')
+require("dotenv").config();
+const Person = require('./models/person')
 
+const password = process.argv[2]
+
+const url = process.env.MONGODB_URI;
+
+mongoose.set('strictQuery',false)
+
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+personSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+// const Person = mongoose.model('Person', personSchema)
+
+const person = new Person({
+  name: process.argv[3],
+  number: process.argv[4],
+})
 
 morgan.token('body', function getId (req) {
   return `${JSON.stringify(req.body)}`
@@ -44,7 +74,10 @@ app.get('/', (req, res) => {
 })
 
 app.get('/api/persons', (req, res) => {
-  res.json(persons)
+  Person.find({}).then(persons => {
+    res.json(persons)
+  })
+  
 })
 
 app.get('/info', (req, res) => {
